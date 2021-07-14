ecc1b85ffe1183996571c253c33225a2f3f84ea0
Dylan Andrada <dylanandrada@Dylans-MacBook-Pro.local>
2021-07-14 06:12:42 -0400

    Added Feature to compose post on home screen as well as connect nav buttons to desired pages

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs")
-
+var posts = [];
 const app =express();
  const homeStartingContent="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 const aboutContent = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
@@ -16,12 +16,41 @@ app.use(express.static("public"));
 
 
 app.get("/",function(req,res){
-  res.render("home",{homeContent:homeStartingContent})
+  console.log(posts);
+  res.render("home",{homeContent:homeStartingContent,composePost:posts})
+
 });
 app.post("/",function(req,res){
 
-})
+});
 
+app.get("/about",function(req,res){
+  res.render("about",{aboutContent:aboutContent})
+});
+app.post("/about",function(req,res){
+
+});
+app.get("/contact",function(req,res){
+  res.render("contact",{contactContentStuff:contactContent});
+});
+app.get("/compose",function(req,res){
+  res.render("compose")
+});
+app.post("/compose",function(req,res){
+
+  const Post ={
+    title:req.body.postTitle,
+    body:req.body.postBody
+  };
+  posts.push(Post);
+  res.redirect("/");
+
+
+
+
+
+});
 app.listen(3000,function(req,res){
   console.log("Listening");
-});
+
+});;
